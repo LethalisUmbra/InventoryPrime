76611ff71f448984a31165489dd4cd33386b02bd
LethalisUmbra <anibal.cabrera2000@gmail.com>
2020-12-29 17:42:05 -0300

    gui improves

--- inventory/static/js/dropdown.js
+++ inventory/static/js/dropdown.js
@@ -1,5 +1,5 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function dropFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+function profileFunction() {
+  document.getElementById("dropdown_profile").classList.toggle("show");
 }
