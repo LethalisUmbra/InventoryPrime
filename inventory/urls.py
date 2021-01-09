9d26ae5bd023a52dc1cbaeaed1a673c2c6e21fa4
LethalisUmbra <anibal.cabrera2000@gmail.com>
2021-01-09 18:11:20 -0300

    Sign Up section and Login improves

--- inventory/urls.py
+++ inventory/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login, name='login'),
     path('logout', LogoutView.as_view(template_name='inventory/home.html'), name='logout'),
+    path('signup/', views.register, name='register'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,19 +1,21 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 
-# Formulario y Autenticación del Usuario
-from django.contrib.auth import login as do_login
-from django.contrib.auth.models import User
+# fomrs and user authentication
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as do_login
 from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+
+# Models
+from .forms import UCFCompleted
 
 def home(request):
 	login(request)
 	return render(request, 'inventory/home.html')
 
 def login(request):
-
-	# codigo para el formulario del login con autenticación
 	login_form = AuthenticationForm()
 	if request.method == "POST" and 'login_btn':
 		login_form = AuthenticationForm(data=request.POST)
@@ -26,3 +28,15 @@ def login(request):
 				do_login(request, user)
 				return redirect('home')
 	return render(request, 'inventory/login.html')
+
+def register(request):
+	login(request)
+	register_form = UCFCompleted()
+
+	if request.method == "POST" and 'register_btn':
+		register_form = UCFCompleted(request.POST)
+		if register_form.is_valid():
+			register_form.save();
+			return HttpResponseRedirect('/')
+
+	return render(request, 'inventory/register.html', {'register': register_form})
