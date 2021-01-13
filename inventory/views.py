aee5d033c804a136f1ede26bf3401f47f65c9416
LethalisUmbra <anibal.cabrera2000@gmail.com>
2021-01-12 21:02:43 -0300

    Error message on Login / Signup

--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
+from django.contrib import messages
 
 # fomrs and user authentication
 from django.contrib.auth.forms import AuthenticationForm
@@ -27,6 +28,9 @@ def login(request):
 			if user is not None:
 				do_login(request, user)
 				return redirect('home')
+		else:
+			messages.error(request, 'Username and password do not match.')
+			return render(request, 'inventory/login.html')
 	return render(request, 'inventory/login.html')
 
 def register(request):
@@ -36,7 +40,14 @@ def register(request):
 	if request.method == "POST" and 'register_btn':
 		register_form = UCFCompleted(request.POST)
 		if register_form.is_valid():
-			register_form.save();
+			user = register_form.save();
+			user = authenticate(username=register_form.cleaned_data['username'],
+                                password=register_form.cleaned_data['password1'],
+                                )
+			do_login(request, user)
 			return HttpResponseRedirect('/')
+		else:
+			messages.error(request, 'Username is not available.')
+			return render(request, 'inventory/register.html', {'register': register_form})
 
 	return render(request, 'inventory/register.html', {'register': register_form})
