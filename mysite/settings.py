8a47acfba06ca6df1559a36cdbf940058ab4e695
LethalisUmbra <anibal.cabrera2000@gmail.com>
2021-01-13 23:37:11 -0300

    Shaping the primary weapons section

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'inventory.apps.InventoryConfig',
+    'primary.apps.PrimaryConfig',
 ]
 
 MIDDLEWARE = [
--- mysite/urls.py
+++ mysite/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('inventory.urls')),
+    path('Primary/', include('primary.urls')),
+    path('primary/', include('primary.urls')),
 ]
