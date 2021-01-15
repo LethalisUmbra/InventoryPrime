5f8cbd771a08a5a45df6fa35721181f09cf14615
LethalisUmbra <anibal.cabrera2000@gmail.com>
2021-01-14 21:31:21 -0300

    optimizing a lot of code on primary models :)

--- inventory/views.py
+++ inventory/views.py
@@ -11,6 +11,7 @@ from django.contrib.auth.models import User
 
 # Models
 from .forms import UCFCompleted
+from primary.models import *
 
 def home(request):
 	login(request)
@@ -45,6 +46,52 @@ def register(request):
                                 password=register_form.cleaned_data['password1'],
                                 )
 			do_login(request, user)
+
+			# Bow Weapons
+			cernos = Bow(name_bow="Cernos Prime", owner=user)
+			cernos.save()
+			paris = Bow(name_bow="Paris Prime", owner=user)
+			paris.save()
+			# Rifle Weaopns
+			baza = Rifle(name_rifle="Baza Prime", owner=user)
+			baza.save()
+			boltor = Rifle(name_rifle="Boltor Prime", owner=user)
+			boltor.save()
+			braton = Rifle(name_rifle="Braton Prime", owner=user)
+			braton.save()
+			burston = Rifle(name_rifle="Burston Prime", owner=user)
+			burston.save()
+			latron = Rifle(name_rifle="Latron Prime", owner=user)
+			latron.save()
+			panthera = Rifle(name_rifle="Panthera Prime", owner=user)
+			panthera.save()
+			soma = Rifle(name_rifle="Soma Prime", owner=user)
+			soma.save()
+			stradavar = Rifle(name_rifle="Stradavar Prime", owner=user)
+			stradavar.save()
+			sybaris = Rifle(name_rifle="Sybaris Prime", owner=user)
+			sybaris.save()
+			tiberon = Rifle(name_rifle="Tiberon Prime", owner=user)
+			tiberon.save()
+
+			# Shotgun Weapons
+			tigris = Shotgun(name_shotgun="Tigris Prime", owner=user)
+			tigris.save()
+			corinth = Shotgun(name_shotgun="Corinth Prime", owner=user)
+			corinth.save()
+			boar = Shotgun(name_shotgun="Boar Prime", owner=user)
+			boar.save()
+
+			# Sniper Weapons
+			vectis = Sniper(name_sniper="Vectis Prime", owner=user)
+			vectis.save()
+			rubico = Sniper(name_sniper="Rubico Prime", owner=user)
+			rubico.save()
+
+			# Zhuge Weapons
+			zhuge = Zhuge(name_zhuge="Zhuge Prime", owner=user)
+			zhuge.save()
+
 			return HttpResponseRedirect('/')
 		else:
 			messages.error(request, 'Username is not available.')
--- primary/models.py
+++ primary/models.py
@@ -1,70 +1,76 @@
 from django.db import models
+from django.conf import settings
 from django.core.validators import *
 
 ################## PRIMARY ##################
+class Bow(models.Model): # Upper Limb, Lower Limb, Grip, String
+	id_bow = models.AutoField(primary_key=True)
+	name_bow = models.CharField(max_length=50, validators=[MaxLengthValidator(150), MinLengthValidator(4)])
+	blueprint_bow = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)], default=0)
+	upper_bow = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)], default=0)
+	lower_bow = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)], default=0)
+	grip_bow = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)], default=0)
+	string_bow = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)], default=0)
+	owned_bow = models.BooleanField(default=False)
+	owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
+
+	def __str__(self):
+		return_string = (self.name_bow + " - " + self.owner.username)
+		return return_string
+
 class Rifle(models.Model): # Barrel, Receiver, Stock
 	id_rifle = models.AutoField(primary_key=True)
 	name_rifle = models.CharField(max_length=50, validators=[MaxLengthValidator(150), MinLengthValidator(4)])
-	blueprint_rifle = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)])
-	barrel_rifle = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)])
-	receiver_rifle = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)])
-	stock_rifle = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)])
+	blueprint_rifle = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)], default=0)
+	barrel_rifle = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)], default=0)
+	receiver_rifle = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)], default=0)
+	stock_rifle = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)], default=0)
 	owned_rifle = models.BooleanField(default=False)
-	user_rifle = models.CharField(max_length=150, validators=[MaxLengthValidator(150), MinLengthValidator(4)])
+	owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
 
 	def __str__(self):
-		return self.name_rifle
+		return_string = (self.name_rifle + " - " + self.owner.username)
+		return return_string
 
 class Shotgun(models.Model): # Barrel, Receiver, Stock
 	id_shotgun = models.AutoField(primary_key=True)
 	name_shotgun = models.CharField(max_length=50, validators=[MaxLengthValidator(150), MinLengthValidator(4)])
-	blueprint_shotgun = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)])
-	barrel_shotgun = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)])
-	receiver_shotgun = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)])
-	stock_shotgun = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)])
+	blueprint_shotgun = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)], default=0)
+	barrel_shotgun = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)], default=0)
+	receiver_shotgun = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)], default=0)
+	stock_shotgun = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)], default=0)
 	owned_shotgun = models.BooleanField(default=False)
-	user_shotgun = models.CharField(max_length=150, validators=[MaxLengthValidator(150), MinLengthValidator(4)])
+	owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
 
 	def __str__(self):
-		return self.name_shotgun
+		return_string = (self.name_shotgun + " - " + self.owner.username)
+		return return_string
 
 class Sniper(models.Model): # Barrel, Receiver, Stock
 	id_sniper = models.AutoField(primary_key=True)
 	name_sniper = models.CharField(max_length=50, validators=[MaxLengthValidator(150), MinLengthValidator(4)])
-	blueprint_sniper = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)])
-	barrel_sniper = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)])
-	receiver_sniper = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)])
-	stock_sniper = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)])
+	blueprint_sniper = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)], default=0)
+	barrel_sniper = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)], default=0)
+	receiver_sniper = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)], default=0)
+	stock_sniper = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)], default=0)
 	owned_sniper = models.BooleanField(default=False)
-	user_sniper = models.CharField(max_length=150, validators=[MaxLengthValidator(150), MinLengthValidator(4)])
+	owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
 
 	def __str__(self):
-		return self.name_sniper
-
-class Bow(models.Model): # Upper Limb, Lower Limb, Grip, String
-	id_bow = models.AutoField(primary_key=True)
-	name_bow = models.CharField(max_length=50, validators=[MaxLengthValidator(150), MinLengthValidator(4)])
-	blueprint_bow = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)])
-	upper_bow = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)])
-	lower_bow = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)])
-	grip_bow = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)])
-	string_bow = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)])
-	owned_bow = models.BooleanField(default=False)
-	user_bow = models.CharField(max_length=150, validators=[MaxLengthValidator(150), MinLengthValidator(4)])
-
-	def __str__(self):
-		return self.name_bow
+		return_string = (self.name_sniper + " - " + self.owner.username)
+		return return_string
 
 class Zhuge(models.Model): # Grip, String, Barrel, Receiver
 	id_zhuge = models.AutoField(primary_key=True)
 	name_zhuge = models.CharField(max_length=50, validators=[MaxLengthValidator(150), MinLengthValidator(4)])
-	blueprint_zhuge = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)])
-	grip_zhuge = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)])
-	string_zhuge = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)])
-	barrel_zhuge = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)])
-	receiver_zhuge = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)])
+	blueprint_zhuge = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)], default=0)
+	grip_zhuge = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)], default=0)
+	string_zhuge = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)], default=0)
+	barrel_zhuge = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)], default=0)
+	receiver_zhuge = models.IntegerField(validators=[MaxValueValidator(999), MinValueValidator(0)], default=0)
 	owned_zhuge = models.BooleanField(default=False)
-	user_zhuge = models.CharField(max_length=150, validators=[MaxLengthValidator(150), MinLengthValidator(4)])
+	owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
 
 	def __str__(self):
-		return self.name_zhuge
+		return_string = (self.name_zhuge + " - " + self.owner.username)
+		return return_string
